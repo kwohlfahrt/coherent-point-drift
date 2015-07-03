64287fb556eee37ca6bc6230dc46de3ef8d2b5c8
Kai Wohlfahrt <kjw53@cam.ac.uk>
2015-07-03 11:34:17 +0100

    Add RMSD for fit error estimate

--- align.py
+++ align.py
@@ -7,6 +7,14 @@ from numpy import exp, trace, diag
 
 from math import pi
 
+def RMSD(X, Y):
+    from numpy import sqrt
+    dist = pairwiseDistanceSquared(X, Y)
+    return sqrt(1/dist.shape[0] * dist.min(axis=0).sum())
+
+def pairwiseDistanceSquared(X, Y):
+    return ((X[None, :, :] - Y[:, None, :]) ** 2).sum(axis=2)
+
 def driftRigid(X, Y, w=0.5):
     if not (X.ndim == Y.ndim == 2):
         raise ValueError("Expecting 2D input data, got {}D and {}D"
@@ -22,8 +30,8 @@ def driftRigid(X, Y, w=0.5):
     N = len(X)
     M = len(Y)
 
-    pairwise_dist_squared = ((X[None, :, :] - Y[:, None, :]) ** 2).sum(axis=2)
-    sigma_squared = 1 / (D*M*N) * pairwise_dist_squared.sum()
+    sigma_squared = 1 / (D*M*N) * pairwiseDistanceSquared(X, Y).sum()
+
     R = eye(D)
     t = zeros(D)
     s = 1.0
@@ -32,7 +40,7 @@ def driftRigid(X, Y, w=0.5):
 
     while True:
         # E-step
-        pairwise_dist_squared = ((X[None, :, :] - (s * R.dot(Y.T).T + t)[:, None, :]) ** 2).sum(axis=2)
+        pairwise_dist_squared = pairwiseDistanceSquared(X, s * R.dot(Y.T).T + t)
         try:
             P = (exp(-1/(2*sigma_squared) * pairwise_dist_squared)
                 / (exp(-1/(2*sigma_squared) * pairwise_dist_squared).sum(axis=0)
@@ -55,12 +63,13 @@ def driftRigid(X, Y, w=0.5):
         R = U.dot(C).dot(VT)
         s = trace(A.T.dot(R)) / trace(Y_hat.T.dot(diag(P.sum(axis=1))).dot(Y_hat))
         t = mu_x - s * R.dot(mu_y)
-        sigma_squared = 1 / (N_p * D) * (trace(X_hat.T.dot(diag(P.T.sum(axis=1))).dot(X_hat)) - s * trace(A.T.dot(R)))
+        sigma_squared = 1 / (N_p * D) * (trace(X_hat.T.dot(diag(P.T.sum(axis=1))).dot(X_hat))
+                                         - s * trace(A.T.dot(R)))
 
         yield R, t, s
 
 if __name__ == "__main__":
-    from math import sin, cos
+    from math import sin, cos, radians
     from numpy.random import rand, seed
     from numpy import array
     from matplotlib import pyplot as plt
@@ -68,22 +77,30 @@ if __name__ == "__main__":
     seed(4)
 
     pt1 = rand(12, 2)
+    plt.scatter(pt1[:, 0], pt1[:, 1], color='blue')
 
-    theta = 80/360 * 2 * pi
-    rotation = array([[cos(theta), -sin(theta)],
-                      [sin(theta), cos(theta)]])
     translation = array([0.1, 0.3])
     scale = 0.5
-    pt2 = rotation.dot(pt1[:10].T).T * scale + translation
 
-    estimate = driftRigid(pt1, pt2, w=0.5)
-    plt.scatter(pt1[:, 0], pt1[:, 1], color='blue')
-    plt.scatter(pt2[:, 0], pt2[:, 1], color='red')
-    for i in range(100):
-        try:
-            R, t, s = next(estimate)
-        except StopIteration:
-            break
-    p_fitted = R.dot(pt2.T).T * s + t
-    plt.scatter(p_fitted[:, 0], p_fitted[:, 1], color='red', marker='+')
+    errors = []
+
+    for theta in range(0, 180, 10):
+        theta = radians(theta)
+        rotation = array([[cos(theta), -sin(theta)],
+                        [sin(theta), cos(theta)]])
+        pt2 = rotation.dot(pt1[:10].T).T * scale + translation
+        plt.scatter(pt2[:, 0], pt2[:, 1], color='red')
+
+        estimate = driftRigid(pt1, pt2, w=0.5)
+        for i in range(100):
+            try:
+                R, t, s = next(estimate)
+            except StopIteration:
+                break
+        p_fitted = R.dot(pt2.T).T * s + t
+        plt.scatter(p_fitted[:, 0], p_fitted[:, 1], color='red', marker='+')
+        errors.append((theta, RMSD(pt1, p_fitted)))
+
+    plt.figure()
+    plt.plot(*zip(*errors))
     plt.show()
