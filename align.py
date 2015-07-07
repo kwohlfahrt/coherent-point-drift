3a6e2196f1cf6fdec5eec907701a94633f12240d
Kai Wohlfahrt <kjw53@cam.ac.uk>
2015-07-07 13:13:08 +0100

    Refactor test
    Test is now a proper script that takes command line arguments, for 2 or 3
    dimensions. Uses multiprocessing to parallelize tests.

--- align.py
+++ align.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 from math import pi
 
 def globalAlignment(X, Y, w=0.9, step=pi/4):
@@ -73,46 +72,3 @@ def driftRigid(X, Y, w=0.9, initial_guess=None):
         sigma_squared = 1 / (N_p * D) * (trace(X_hat.T.dot(diag(P.T.sum(axis=1))).dot(X_hat))
                                          - s * trace(A.T.dot(R)))
         yield R, t, s
-
-if __name__ == "__main__":
-    from math import sin, cos, pi, degrees
-    from numpy.random import rand, seed, shuffle
-    from numpy import array, eye
-    from matplotlib import pyplot as plt
-    from geometry import rotationMatrix, RMSD, rigidXform
-
-    seed(4)
-
-    N = 12
-    drop = 2
-    D = 2
-    repeats = 2
-
-    reference = rand(N, D)
-    plt.figure()
-    plt.scatter(reference[:, 0], reference[:, 1], marker='v', color='black')
-
-    colors = rand(repeats, 3)
-    errors = []
-
-    for i in range(repeats):
-        shuffle(reference) # To remove different points
-        if D == 3:
-            rotation = rotationMatrix(*(rand(3) * 2 * pi))
-        elif D == 2:
-            rotation = rotationMatrix(rand() * 2 * pi)
-        scale = rand() + 0.5
-        translation = rand(D)
-        color = rand(3)
-
-        moved = rigidXform(reference[:N-drop], rotation, translation, scale)
-        plt.scatter(moved[:, 0], moved[:, 1], marker='o', color=colors[i], alpha=0.5)
-
-        R, t, s = globalAlignment(reference, moved, w=0.9, step=pi/8)
-        fitted = rigidXform(moved, R, t, s)
-        plt.scatter(fitted[:, 0], fitted[:, 1], marker='+', color='green')
-        errors.append(RMSD(reference, fitted))
-
-    plt.figure()
-    plt.scatter(range(len(errors)), errors, color=colors)
-    plt.show()
---
+++ test_rigid.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+from math import pi
+
+def tryAlignment(reference, R, t, s):
+    from geometry import rigidXform, rotationMatrix
+    from align import globalAlignment
+
+    moved = rigidXform(reference, rotationMatrix(*R), t, s)
+    return globalAlignment(reference, moved, 0.9, pi/6)
+
+if __name__ == '__main__':
+    from argparse import ArgumentParser
+    from numpy.random import rand, seed, shuffle
+    from multiprocessing import Pool
+    from itertools import repeat
+
+    parser = ArgumentParser("Test random data for 2D and 3D alignment")
+    parser.add_argument('N', type=int, help='Number of points')
+    parser.add_argument('D', type=int, choices=(2, 3), help='Number of dimensions')
+    parser.add_argument('repeats', type=int, help='Number of trials to run')
+    parser.add_argument('--drop', default=0, type=int,
+                        help='number of points to exclude from the reference set')
+
+    parser.add_argument('--rotate', nargs=2, type=float, default=(-pi, pi),
+                        help='The range of rotations to test')
+    parser.add_argument('--translate', nargs=2, type=float, default=(-1.0, 1.0),
+                        help='The range of translations to test')
+    parser.add_argument('--scale', nargs=2, type=float, default=(0.5, 1.5),
+                        help='The range of scales to test')
+
+    parser.add_argument('--plot', action='store_true')
+    parser.add_argument('--pickle', type=str)
+
+    seed(4)
+
+    args = parser.parse_args()
+    reference = rand(args.N, args.D)
+
+    # Could generate per-process if memory is an issue
+    if args.D == 2:
+        rotations = rand(args.repeats, 1)
+    if args.D == 3:
+        rotations = rand(args.repeats, 3)
+    rotations = rotations * (args.rotate[1] - args.rotate[0]) + args.rotate[0]
+    translations = (rand(args.repeats, args.D) * (args.translate[1] - args.translate[0])
+                    + args.translate[0])
+    scales = rand(args.repeats) * (args.scale[1] - args.scale[0]) + args.scale[0]
+
+    with Pool() as p:
+        # Pool only supports one argument for map, so use starmap + zip
+        xforms = p.starmap(tryAlignment, zip(repeat(reference), rotations, translations, scales))
+
+    if args.pickle:
+        import pickle
+        from pathlib import Path
+        pickle_path = Path(args.pickle)
+        with pickle_path.open('wb') as f:
+            pickle.dump(reference, f)
+            pickle.dump(xforms, f)
+            pickle.dump(rotations, f)
+            pickle.dump(translations, f)
+            pickle.dump(scales, f)
+
+    if args.plot:
+        import matplotlib.pyplot as plt
+        from geometry import rigidXform, rotationMatrix
+
+        plt.scatter(reference[:, 0], reference[:, 1], marker='v', color='black')
+        for xform, R, t, s in zip(xforms, rotations, translations, scales):
+            moved = rigidXform(reference, rotationMatrix(*R), t, s)
+            color = rand(3)
+            plt.scatter(moved[:, 0], moved[:, 1], marker='o', color=color, alpha=0.5)
+            fitted =rigidXform(moved, *xform)
+            plt.scatter(fitted[:, 0], fitted[:, 1], marker='+', color=color)
+        plt.show()
