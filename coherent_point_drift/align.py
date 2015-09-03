a9909b1b013b74f20fdc2482122d034c1980f67a
Kai Wohlfahrt <kjw53@cam.ac.uk>
2015-09-03 15:36:07 +0100

    Remove pre-normalization, use standard deviation
    The pre-normalization was an extra step, and would have required handling
    differently with affine/drift transforms.
    
    Second, use the standard deviation (i.e. normalize to same variance) for the
    scale estimate. This in some cases reduces the range of 'w' over which an
    alignment converges. 'w' generally needs to be smaller than before, as the
    standard deviation is smaller than the span.

--- coherent_point_drift/align.py
+++ coherent_point_drift/align.py
@@ -5,9 +5,10 @@ def tryAlignment(X, Y, w, maxiter, rotation):
     from itertools import islice
     from .geometry import rotationMatrix
 
-    return last(islice(driftRigid(X, Y, w, rotationMatrix(*rotation)), maxiter))
+    initial_guess = rotationMatrix(*rotation), None, None
+    return last(islice(driftRigid(X, Y, w, initial_guess), maxiter))
 
-def globalAlignment(X, Y, w=0.7, nsteps=12, maxiter=200):
+def globalAlignment(X, Y, w=0.5, nsteps=12, maxiter=200):
     from .geometry import spacedRotations, RMSD, rigidXform
     from functools import partial
     from multiprocessing import Pool
@@ -20,9 +21,9 @@ def globalAlignment(X, Y, w=0.7, nsteps=12, maxiter=200):
     return solution
 
 # X is the reference, Y is the points
-def driftRigid(X, Y, w=0.7, initial_R=None):
+def driftRigid(X, Y, w=0.5, initial_guess=(None, None, None)):
     from numpy.linalg import svd, det, norm
-    from numpy import exp, trace, diag
+    from numpy import exp, trace, diag, std
     from numpy import eye, zeros
     from numpy import seterr
     from math import pi
@@ -42,17 +43,13 @@ def driftRigid(X, Y, w=0.7, initial_R=None):
     N = len(X)
     M = len(Y)
 
-    # Pre-normalize input data
-    pre_offsets = tuple(map(lambda x: -x.mean(axis=0), (X, Y)))
-    X = X + pre_offsets[0]
-    Y = Y + pre_offsets[1]
-    pre_scales = tuple(map(lambda x: 1 / abs(x).max(), (X, Y)))
-    X = X * pre_scales[0]
-    Y = Y * pre_scales[1]
-
-    R = initial_R if initial_R is not None else eye(D)
-    t = 0
-    s = 1
+    R, t, s = initial_guess
+    if R is None:
+        R = eye(D)
+    if s is None:
+        s = std(X) / std(R.dot(Y.T).T)
+    if t is None:
+        t = X.mean(axis=0) - rigidXform(Y, R, 0, s).mean(axis=0)
 
     sigma_squared = 1 / (D*M*N) * pairwiseDistanceSquared(rigidXform(Y, R, t, s), X).sum()
     old_exceptions = seterr(divide='ignore', over='ignore', under='ignore', invalid='raise')
@@ -60,10 +57,12 @@ def driftRigid(X, Y, w=0.7, initial_R=None):
         # E-step
         pairwise_dist_squared = pairwiseDistanceSquared(rigidXform(Y, R, t, s), X)
         try:
+            # The original algorithm expects unit variance, so normalize (2πς**2)**D/2 to compensate
+            # No other parts are scale-dependent
             P = (exp(-1/(2*sigma_squared) * pairwise_dist_squared)
                 / (exp(-1/(2*sigma_squared) * pairwise_dist_squared).sum(axis=0)
                     + (2 * pi * sigma_squared) ** (D/2)
-                    * w / (1-w) * M / N))
+                    * w / (1-w) * M / N / std(X) ** D))
         except FloatingPointError:
             seterr(**old_exceptions)
             break
@@ -83,9 +82,4 @@ def driftRigid(X, Y, w=0.7, initial_R=None):
         t = mu_x - s * R.dot(mu_y)
         sigma_squared = 1 / (N_p * D) * (trace(X_hat.T.dot(diag(P.T.sum(axis=1))).dot(X_hat))
                                          - s * trace(A.T.dot(R)))
-
-        # Compensate for input normalization
-        scale_out = s * pre_scales[1] / pre_scales[0]
-        translation_out = (s * pre_scales[1] * R.dot(pre_offsets[1]) / pre_scales[0]
-                           + t / pre_scales[0] - pre_offsets[0])
-        yield R, translation_out, scale_out
+        yield R, t, s
