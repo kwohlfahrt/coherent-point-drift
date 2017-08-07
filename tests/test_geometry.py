27bd787e6777ace94079ec055a6f967cb019c8b2
Kai Wohlfahrt <kjw53@cam.ac.uk>
2017-08-07 14:42:32 +0100

    Add RMSD smoke-test

--- tests/test_geometry.py
+++ tests/test_geometry.py
@@ -10,3 +10,10 @@ def test_random_rotations():
         np.testing.assert_equal(*islice(randomRotations(3), 2))
     with pytest.raises(AssertionError):
         np.testing.assert_equal(next(randomRotations(3)), next(randomRotations(3)))
+
+def test_rmsd():
+    points = np.random.uniform(0, 1, (10, 2))
+    P = np.eye(len(points))
+
+    assert RMSD(points, points + 1, P) > 1
+    assert RMSD(points, points, P) == 0
