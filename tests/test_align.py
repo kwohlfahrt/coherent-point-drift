d52fef38dcb60e9ab9cb1e351c6f263eb00cbf96
Kai Wohlfahrt <kjw53@cam.ac.uk>
2017-08-15 17:25:33 +0100

    Store fixture points as text
    Easier to compare to reference implementation

--- tests/test_align.py
+++ tests/test_align.py
@@ -1,27 +1,26 @@
 from coherent_point_drift.align import *
 from coherent_point_drift.util import last
 from coherent_point_drift.geometry import RMSD, rigidXform
+from coherent_point_drift.main import loadPoints, loadXform
 from pickle import load
+from pathlib import Path
 
 import numpy as np
 import pytest
 from itertools import islice
 
 def test_global():
-    with open("tests/fixtures/ref.pickle", "rb") as f:
-        X = load(f)
-    with open("tests/fixtures/deg.pickle", "rb") as f:
-        Y = load(f)
-    with open("tests/fixtures/xform.pickle", "rb") as f:
-        expected = load(f)
+    X = loadPoints(Path("tests/fixtures/ref.txt"))
+    Y = loadPoints(Path("tests/fixtures/deg.txt"))
+    expected = loadXform(Path("tests/fixtures/xform.pickle"))
+
     P, xform = globalAlignment(X, Y, w=0.5, mirror=True)
 
     for r, e in zip(xform, expected):
         np.testing.assert_almost_equal(r, e)
 
-def test_perfect():
-    with open("tests/fixtures/ref.pickle", "rb") as f:
-        X = load(f)
+def test_perfect_rigid():
+    X = loadPoints(Path("tests/fixtures/ref.txt"))
     P, (R, t, s) = last(islice(driftRigid(X, X), 100))
 
     np.testing.assert_almost_equal(R, np.eye(2))
--- tests/test_entry.py
+++ tests/test_entry.py
@@ -1,7 +1,9 @@
 from pickle import load, loads
 from subprocess import run, PIPE
 from numpy.testing import assert_almost_equal
+from coherent_point_drift.main import loadPoints, loadXform
 from shlex import split
+from pathlib import Path
 
 import pytest
 import numpy as np
@@ -9,9 +11,8 @@ import numpy as np
 cmd = ["python3", "-m" "coherent_point_drift.main"]
 
 def test_align():
-    args = split("align tests/fixtures/ref.pickle tests/fixtures/deg.pickle --format pickle")
-    with open("tests/fixtures/xform.pickle", "rb") as f:
-        expected = load(f)
+    args = split("align tests/fixtures/ref.txt tests/fixtures/deg.txt --format pickle")
+    expected = loadXform(Path("tests/fixtures/xform.pickle"))
 
     r = run(cmd + args, stdout=PIPE, universal_newlines=False)
     assert not r.returncode
@@ -21,18 +22,17 @@ def test_align():
         assert_almost_equal(r, e)
 
 def test_xform_inverse(tmpdir):
-    with open("tests/fixtures/ref.pickle", "rb") as f:
-        expected = load(f)
+    expected = loadPoints(Path("tests/fixtures/ref.txt"))
 
     # Generate degraded points (without noise)
-    args = split("xform tests/fixtures/ref.pickle tests/fixtures/xform.pickle --format pickle")
+    args = split("xform tests/fixtures/ref.txt tests/fixtures/xform.pickle --format txt")
     r = run(cmd + args, stdout=PIPE, universal_newlines=False)
     assert not r.returncode
-    deg = tmpdir.join("deg.pickle")
+    deg = tmpdir.join("deg.txt")
     deg.write_binary(r.stdout)
 
     # Generate alignment
-    args = split("align tests/fixtures/ref.pickle '{}' --format pickle".format(str(deg)))
+    args = split("align tests/fixtures/ref.txt '{}' --format pickle".format(str(deg)))
     r = run(cmd + args, stdout=PIPE, universal_newlines=False)
     assert not r.returncode
     xform = tmpdir.join("xform.pickle")
@@ -47,29 +47,27 @@ def test_xform_inverse(tmpdir):
 
 
 def test_print(tmpdir):
-    with open("tests/fixtures/ref.pickle", "rb") as f:
-        expected = load(f)
+    expected = loadPoints(Path("tests/fixtures/ref.txt"))
 
     # Generate degraded points (without noise)
-    args = split("xform tests/fixtures/ref.pickle tests/fixtures/xform.pickle --format pickle")
+    args = split("xform tests/fixtures/ref.txt tests/fixtures/xform.pickle --format txt")
     r = run(cmd + args, stdout=PIPE, universal_newlines=False)
     assert not r.returncode
-    deg = tmpdir.join("deg.pickle")
+    deg = tmpdir.join("deg.txt")
     deg.write_binary(r.stdout)
 
     # Generate alignment
-    args = split("align tests/fixtures/ref.pickle '{}' --format print".format(str(deg)))
+    args = split("align tests/fixtures/ref.txt '{}' --format print".format(str(deg)))
     r = run(cmd + args, stdout=PIPE, universal_newlines=False)
     assert not r.returncode
 
 def test_align_multiple():
     args = split(
-        "align tests/fixtures/ref.pickle tests/fixtures/deg.pickle "
-        "tests/fixtures/ref.pickle tests/fixtures/deg.pickle "
+        "align tests/fixtures/ref.txt tests/fixtures/deg.txt "
+        "tests/fixtures/ref.txt tests/fixtures/deg.txt "
         "-w {w} --format pickle".format(w=2/3)
     )
-    with open("tests/fixtures/xform.pickle", "rb") as f:
-        expected = load(f)
+    expected = loadXform(Path("tests/fixtures/xform.pickle"))
 
     r = run(cmd + args, stdout=PIPE, universal_newlines=False)
     assert not r.returncode
@@ -80,8 +78,8 @@ def test_align_multiple():
 
 def test_align_multiple_w():
     args = split(
-        "align tests/fixtures/ref.pickle tests/fixtures/deg.pickle "
-        "tests/fixtures/ref.pickle tests/fixtures/deg.pickle "
+        "align tests/fixtures/ref.txt tests/fixtures/deg.txt "
+        "tests/fixtures/ref.txt tests/fixtures/deg.txt "
         "-w 0.5 0.8 --format pickle"
     )
 
@@ -90,8 +88,8 @@ def test_align_multiple_w():
 
 def test_plot_multiple(tmpdir):
     args = split(
-        "plot tests/fixtures/ref.pickle tests/fixtures/deg.pickle "
-        "tests/fixtures/ref.pickle tests/fixtures/deg.pickle "
+        "plot tests/fixtures/ref.txt tests/fixtures/deg.txt "
+        "tests/fixtures/ref.txt tests/fixtures/deg.txt "
         "tests/fixtures/xform.pickle --outfile {}".format(tmpdir.join('test.pdf'))
     )
     r = run(cmd + args, stdout=PIPE, universal_newlines=False)
@@ -99,12 +97,11 @@ def test_plot_multiple(tmpdir):
 
 def test_global_multiple():
     args = split(
-        "align tests/fixtures/ref.pickle tests/fixtures/deg.pickle "
-        "tests/fixtures/ref.pickle tests/fixtures/deg.pickle "
+        "align tests/fixtures/ref.txt tests/fixtures/deg.txt "
+        "tests/fixtures/ref.txt tests/fixtures/deg.txt "
         "--format pickle --scope global -w {w}".format(w=2/3)
     )
-    with open("tests/fixtures/xform.pickle", "rb") as f:
-        expected = load(f)
+    expected = loadXform(Path("tests/fixtures/xform.pickle"))
 
     r = run(cmd + args, stdout=PIPE, universal_newlines=False)
     assert not r.returncode
@@ -116,8 +113,8 @@ def test_global_multiple():
 def test_plot_sizes(tmpdir):
     for sizes in ["1.0", "0.5 1.0"]:
         args = split(
-            "plot tests/fixtures/ref.pickle tests/fixtures/deg.pickle "
-            "tests/fixtures/ref.pickle tests/fixtures/deg.pickle "
+            "plot tests/fixtures/ref.txt tests/fixtures/deg.txt "
+            "tests/fixtures/ref.txt tests/fixtures/deg.txt "
             "tests/fixtures/xform.pickle --sizes {} --outfile {}"
             .format(sizes, tmpdir.join('test.pdf'))
         )
