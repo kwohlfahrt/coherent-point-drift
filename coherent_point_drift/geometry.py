fce3154511a22547e73ffc9fc8146290b4aece6f
Kai Wohlfahrt <kjw53@cam.ac.uk>
2017-08-31 15:02:56 +0100

    Remove RMSD normalization
    This means the 'w' parameter has a greater effect on RMSD weighting.

--- coherent_point_drift/geometry.py
+++ coherent_point_drift/geometry.py
@@ -3,9 +3,7 @@ from functools import partial
 
 def RMSD(X, Y, P):
     from numpy import sqrt
-
-    P = P / P.sum(axis=0, keepdims=True)
-    return sqrt((pairwiseDistanceSquared(X, Y) * P).mean()) / std(X)
+    return sqrt((pairwiseDistanceSquared(X, Y) * P).mean())
 
 def pairwiseDistanceSquared(X, Y):
     # R[i, j] = distance(X[i], Y[j]) ** 2
